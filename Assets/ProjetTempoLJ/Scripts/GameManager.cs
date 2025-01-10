88f909b92140e79092bff7abf8f384ffb01b22eb
Pierre Rossel <>
2025-01-10 17:16:20 +0100

    namespace TempoLJ

--- Assets/ProjetTempoLJ/Scripts/GameManager.cs
+++ Assets/ProjetTempoLJ/Scripts/GameManager.cs
@@ -1,38 +1,42 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour
+namespace TempoLJ
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    public class GameManager : MonoBehaviour
     {
-        Fob.onFinish.AddListener(OnFobFinish);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    void OnFobFinish()
-    {
-        Debug.Log("YEAH ! All fobs have been destroyed");
-
-        // Load the next scene
-        LoadNextScene();
-    }
-
-    void LoadNextScene()
-    {
-        Debug.Log("GameManager : LoadNextScene");
-
-        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
-
-        if (nextSceneIndex >= SceneManager.sceneCountInBuildSettings)
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
         {
-            nextSceneIndex = 0; // Home screen
+            Fob.onFinish.AddListener(OnFobFinish);
         }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void OnFobFinish()
+        {
+            Debug.Log("YEAH ! All fobs have been destroyed");
+
+            // Load the next scene
+            LoadNextScene();
+        }
+
+        void LoadNextScene()
+        {
+            Debug.Log("GameManager : LoadNextScene");
+
+            int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+
+            if (nextSceneIndex >= SceneManager.sceneCountInBuildSettings)
+            {
+                nextSceneIndex = 0; // Home screen
+            }
             SceneManager.LoadScene(nextSceneIndex);
+        }
     }
 }
--- Assets/ProjetTempoLJ/Scripts/RandomRotate.cs
+++ Assets/ProjetTempoLJ/Scripts/RandomRotate.cs
@@ -1,17 +1,21 @@
 using UnityEngine;
 
-public class RandomRotate : MonoBehaviour
+namespace TempoLJ
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public class RandomRotate : MonoBehaviour
     {
-        //rotation continue sur tous les axes
-        transform.Rotate(new Vector3(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360)) * Time.deltaTime);
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //rotation continue sur tous les axes
+            transform.Rotate(new Vector3(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360)) * Time.deltaTime);
+        }
     }
 }
